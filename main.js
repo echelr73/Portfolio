c496443d6878819c0a54646204992adb5c784eeb
Martin Echeverria <martin.echeverria@accenture.com>
2023-06-09 17:57:15 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,147 @@
+import './style.css'
+import * as THREE from 'three';
+//import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+
+let textMesh, pControl, scene, camera, renderer, clock, direction_light, portalLight, portalLight1;
+let portalParticles = [], smokeParticles = [];
+
+function initScene() {
+  scene = new THREE.Scene();
+  //scene.background = new THREE.Color(0x000000);
+  //scene.fog = new THREE.Fog(0x000000, 0, 10000);
+  camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 1000;
+
+  // Set up the scene lights
+  direction_light = new THREE.DirectionalLight(0xffffff, 0.2);
+  direction_light.position.set(0, 0, 1);
+  scene.add(direction_light);
+
+  portalLight1 = new THREE.PointLight(0xffffff, 4, 0, 2);
+  portalLight1.position.set(0, 0, 100);
+  scene.add(portalLight1);
+
+  portalLight = new THREE.PointLight(0x062d89, 100, 450, 1.7);
+  portalLight.position.set(0, 0, 250);
+  scene.add(portalLight);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x000000, 1);
+  renderer.setSize(window.innerWidth, window.innerHeight - 2);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  document.body.appendChild(renderer.domElement);
+  particleSetup();
+}
+function particleSetup() {
+  let portalLoader = new THREE.TextureLoader();
+
+  portalLoader.load("/humo3.png", function (texture) {
+    let portalGeo = new THREE.PlaneGeometry(200, 200);
+    let portalMaterial = new THREE.MeshStandardMaterial({
+      map: texture,
+      transparent: true
+    });
+
+    let smokeGeo = new THREE.PlaneGeometry(700, 700);
+    let smokeMaterial = new THREE.MeshStandardMaterial({
+      map: texture,
+      transparent: true
+    });
+
+    for (let index = 945; index > 140; index--) {
+      let particle = new THREE.Mesh(portalGeo, portalMaterial);
+      particle.position.set(
+        0.5 * index * Math.cos((4 * index * Math.PI) / 180),
+        0.5 * index * Math.sin((4 * index * Math.PI) / 180),
+        0.1 * index
+      );
+      particle.rotation.z = Math.random() * 360;
+      portalParticles.push(particle);
+      scene.add(particle);
+
+
+    }
+    for (let index = 0; index < 40; index++) {
+      let particle = new THREE.Mesh(smokeGeo, smokeMaterial);
+      particle.position.set(
+        Math.random() * 1000 - 500,
+        Math.random() * 800 - 500,
+        25
+      );
+      particle.rotation.z = Math.random() * 360;
+      particle.material.opacity = 0.2;
+      smokeParticles.push(particle);
+      scene.add(particle);
+    }
+    clock = new THREE.Clock();
+  });
+
+}
+initScene();
+
+const loader = new FontLoader();
+
+loader.load('https://threejs.org/examples/fonts/optimer_regular.typeface.json', function (font) {
+
+  const textGeometry = new TextGeometry('Project X', {
+    font: font,
+    size: 20,
+    height: 1
+  });
+  const textMaterial = new THREE.MeshLambertMaterial({ color: 0xff0fff });
+  textMesh = new THREE.Mesh(textGeometry, textMaterial);
+  textMesh.position.set(-52.5, 0, -1000);
+  scene.add(textMesh);
+});
+
+window.addEventListener('resize', onWindowResize);
+
+// Variables de rebote
+let cant = 0;
+function animate() {
+  requestAnimationFrame(animate);
+
+  let delta = clock.getDelta();
+  portalParticles.forEach(p => {
+    p.rotation.z -= delta * 1.5;
+  });
+  smokeParticles.forEach(p => {
+    p.rotation.z -= delta * 0.2;
+  });
+
+  if (Math.random() > 0.9)
+    portalLight.power = 350 + Math.random() * 500;
+
+  if (textMesh.position.z <= 950) {
+    textMesh.position.z += (cant > 0 && cant<= 1) ? 0.8 : (cant >1 && cant <=2) ? 0.5 : 5.5;
+  }else if (cant < 2){
+    cant++;
+    textMesh.position.z = 935
+  }
+
+  //camera.lookAt(space.position);
+  renderer.render(scene, camera);
+}
+animate();
+
+
+if (WebGL.isWebGLAvailable()) {
+  animate();
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById('containerLeft').appendChild(warning);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+
